29009c0180277e008739a47bf13f0c4ce33e5683
Rik Volger <rvolger@gmail.com>
2025-10-27 11:10:37 +0100

    Update writers to make folders, add tif writer

--- src/tri_ct_tools/image/writer.py
+++ src/tri_ct_tools/image/writer.py
@@ -1,20 +1,34 @@
+import numpy as np
 from pathlib import Path
 from matplotlib.figure import Figure
+import tifffile
 
 
 def print_saving(file):
     print(f"Saving figure to {file}")
 
 
-def save_png(fig: Figure, output_folder: Path, filename: str, dpi=900):
+def fig_png(fig: Figure, output_folder: Path, filename: str, dpi=900):
+    output_folder.mkdir(parents=True, exist_ok=True)
     save_file = output_folder / filename
     save_file_png = save_file.with_suffix(".png")
     print_saving(save_file_png)
     fig.savefig(save_file_png, dpi=dpi)
+    return save_file
 
 
-def save_svg(fig: Figure, output_folder: Path, filename: str):
+def fig_svg(fig: Figure, output_folder: Path, filename: str):
+    output_folder.mkdir(parents=True, exist_ok=True)
     save_file = output_folder / filename
     save_file_svg = save_file.with_suffix(".svg")
     print_saving(save_file_svg)
     fig.savefig(save_file_svg, transparent=True)
+    return save_file
+
+
+def array_to_tif(img: np.ndarray, output_folder: Path, filename: str):
+    output_folder.mkdir(parents=True, exist_ok=True)
+    output_file = output_folder / filename
+    output_tif = output_file.with_suffix(".tif")
+    tifffile.imwrite(output_tif, img)
+    return output_tif

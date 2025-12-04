18cfd6de2460de415d17a654353172608d82a04e
Rik Volger <rvolger@gmail.com>
2025-12-04 08:44:32 +0100

    small bits

--- src/tri_ct_tools/preprocess/dead_pixels.py
+++ src/tri_ct_tools/preprocess/dead_pixels.py
@@ -328,7 +328,7 @@ def pick_dark(source_dir, darks):
 
 def main(root_source_dir, root_target_dir):
     # initial processing of raw data (dead pixel correction, rotate, flip, contrast)
-    with open('inputs/dead_pixel_multisource.yaml') as dp_file:
+    with open('e:/Rik/dead_pixel_multisource_Rik and Jule.yaml') as dp_file:
         config = yaml.safe_load(dp_file)
     if not root_source_dir:
         root_source_dir = config['input_folder']
